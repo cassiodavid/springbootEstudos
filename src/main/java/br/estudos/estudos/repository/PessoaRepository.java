b3425dacb97ceb110e5da7a10e097f1f74779aa8
Cassio David <cassiodavid93@gmail.com>
2020-09-20 00:50:09 -0300

    estudos Spring

---
+++ src/main/java/br/estudos/estudos/repository/PessoaRepository.java
@@ -0,0 +1,8 @@
+package br.estudos.estudos.repository;
+
+import br.estudos.estudos.entity.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long>  { }
